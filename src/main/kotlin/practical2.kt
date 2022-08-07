c3f993e7455136facc47934ca2a4337d355b824e
Karm Patel <karmhpatel20@gnu.ac.in>
2022-08-07 14:41:12 +0530

    practical2

---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,15 @@
+
+fun main()
+{
+    print("Integer Value : ")
+    var i = readLine()!!.toDouble()
+    println("Double Value (From Integer) : $i")
+
+    print("String Value : ")
+    var j = readLine()!!.toInt()
+    println("Integer value(From String) : $j")
+
+    print("String value : ")
+    var k = readLine()!!.toDouble()
+    println("Double Value(From String) : $k ")
+}
