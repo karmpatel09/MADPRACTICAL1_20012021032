bc4b54d38b87e9a01f6b691881ee0ab7cab54804
Karm Patel <karmhpatel20@gnu.ac.in>
2022-08-07 14:30:32 +0530

    Initial commit

---
+++ src/main/kotlin/PRACTICAL1.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    var x : Int=22
+    println("Integer Value : $x ")
+    var y = 1.5
+    println("Float Value : $y ")
+    var z : Char= 'T'
+    println("Character Value : $z ")
+    var s : String="HMS"
+    println("String Value : $s ")
+    var b : Boolean= false
+    println("Boolean value : $b ")
+    var d : Double=96.36
+    println("Double Value : $d ")
+    var l : Long = 338927847
+    println("Long Value : $l ")
+    var h : Short = -2
+    println("Short Value : $h ")
+    var a : Byte = 127
+    println("Byte Value : $a ")
+
+}
