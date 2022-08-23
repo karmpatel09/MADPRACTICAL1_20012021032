1b4d9853cd2c16e2d0a27d02a3ea39ad604d0329
Karm Patel <karmhpatel20@gnu.ac.in>
2022-08-23 21:17:14 +0530

    practical5.kt

---
+++ src/main/kotlin/practical5.kt
@@ -0,0 +1,20 @@
+fun main() {
+    print("Write the Month Number: ")
+    var m: Int = readln()!!.toInt()
+
+    when (m) {
+        1 -> print("January")
+        2 -> print("February")
+        3 -> print("March")
+        4 -> print("April")
+        5 -> print("May")
+        6 -> print("June")
+        7 -> print("July")
+        8 -> print("August")
+        9 -> print("September")
+        10 -> print("October")
+        11 -> print("November")
+        12 -> print("December")
+        else -> print("Enter prefect number")
+    }
+}
